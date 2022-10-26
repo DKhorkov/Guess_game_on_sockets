5546605db0577a7919a2e0eca40207cda31abe69
DKhorkov <dkhorkov.dev@gmail.com>
2022-10-26 23:35:50 +0300

    Refactored code and writed doc-strings

--- client.py
+++ client.py
@@ -12,10 +12,43 @@ class Client:
         self.number_chosen = False
 
     def connect_to_server(self):
+        """Функция, устанавливающая соединение с сервером."""
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client.connect((self.address, self.port))
 
+    def p1_cycle(self):
+        """Цикл для первого игрока. Он загадывает число и отправляет его на сервер. Далее ждет, когда второй игрок
+        попробует отгадать число. Если он угадывает, получаем от сервера положительный ответ -> меняемся местами
+        со вторым игроком. Если ответ отрицательный - ждем очередной попытки от второго игрока."""
+        while True:
+            print('\nWaiting for 2nd player to guess the number...')
+            message = self.client.recv(2048).decode('utf-8')
+            if message == 'True':
+                print('Another player guessed the num')
+                self.player_number, self.number_of_players = self.client.recv(2048).decode('utf-8').split(',')
+                print("Now u'r player 2!\n")
+                break
+            print("Second player didn't guess the correct num\n")
+
+    def p2_cycle(self):
+        """Цикл для второго игрока. Он пробует угадать загаданное первым игроком число и отправляет его на сервер.
+        Далее ждет от сервера ответа. Если ответ положительный, то второй игрок победил -> Ждем ответа от сервера,
+        чтобы поменяться местами. Если ответ положительный - пытаемся угадать снова."""
+        while True:
+            guess = input('Guess num: ').encode('utf-8')
+            self.client.send(guess)
+            message = self.client.recv(2048).decode('utf-8')
+            if message == 'True':
+                print('\nU won')
+                self.player_number, self.number_of_players = self.client.recv(2048).decode('utf-8').split(',')
+                print("Now u'r player 1!")
+                break
+            print('\nNope, try again')
+
     def main(self):
+        """Основная функция клиентской части. Подключается к серверу и отправляет стартовое сообщение. Далее получает
+        номер игрока и количество игроков в игре. Если мы единственный игрок, отправляем запрос на обновлении информации
+        об игроках на сервер. Если игроков двое - начинается игра."""
         self.connect_to_server()
         self.client.send('__join_server'.encode('utf-8'))
         self.player_number, self.number_of_players = self.client.recv(2048).decode('utf-8').split(',')
@@ -24,31 +57,16 @@ class Client:
             if int(self.player_number) == 1 and int(self.number_of_players) == 2:
                 number_to_guess = input('Enter num to guess: ').encode('utf-8')
                 self.client.send(number_to_guess)
-                while True:
-                    print('Waiting for 2nd player to guess the number...')
-                    message = self.client.recv(2048).decode('utf-8')
-                    if message == 'True':
-                        print('Another player guessed the num')
-                        self.player_number, self.number_of_players = self.client.recv(2048).decode('utf-8').split(',')
-                        print("Now u'r player 2!")
-                        break
-                    print("Second player didn't guess the correct num")
+                self.p1_cycle()
+
             elif int(self.player_number) == 2 and int(self.number_of_players) == 2:
                 print('Waiting for 1st player to choose the number...')
                 self.number_chosen = bool(self.client.recv(2048).decode('utf-8'))
-                print('Number was chosen! Now, try to guess it!')
-                while True:
-                    guess = input('Guess num: ').encode('utf-8')
-                    self.client.send(guess)
-                    message = self.client.recv(2048).decode('utf-8')
-                    if message == 'True':
-                        print('u won')
-                        self.player_number, self.number_of_players = self.client.recv(2048).decode('utf-8').split(',')
-                        print("Now u'r player 1!")
-                        break
-                    print('Nope, try again')
+                print('\nNumber was chosen! Now, try to guess it!')
+                self.p2_cycle()
+
             else:
-                print('Waiting for second player')
+                print('Waiting for second player...')
                 self.client.send('get_data'.encode('utf-8'))
                 sleep(5)
                 self.player_number, self.number_of_players = self.client.recv(2048).decode('utf-8').split(',')
--- server.py
+++ server.py
@@ -15,17 +15,45 @@ class Server:
         self.send_num = True
 
     def check_client_on_existence(self, client_addr):
+        """Функция проверяет, есть ли игрок в списке игроков. Если игрок новый - то он добавляется в список и информация
+        об этом выводится в консоль сервера."""
         if client_addr not in self.players:
             self.players.append(client_addr)
             print(self.players)
 
     def create_server(self):
+        """Функция создает сервер и начинает ждать подключения пользователей."""
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((self.address, self.port))
         print(f'Server with address "{self.address}" and port "{self.port}" is running and waiting for connecting')
 
+    def play(self):
+        """Основная функция, запускающая игровую логику. Получаем загаданное число и обновляем атрибуты игры (кол-во
+        попыток, а также передаем загаданное число в игру) и отправляем уведомление второму игроку, что число загадано
+        первым игроком. Пока попытка отгадать не равна загаданному числу, запрашиваем его у второго игрока. Если второй
+        игрок угадал число, уведомляем игроков об этом и меняем их местами (1 становится 2, а 2 становится 1) и
+        уведомляем об этом. Затем игра повторяется."""
+        while True:
+            self.number = self.server.recv(2048).decode('utf-8')
+            self.game.reset_attributes(self.number)
+            self.guess = None
+            self.server.sendto('True'.encode('utf-8'), self.players[1])
+            while not self.number == self.guess:
+                self.guess = self.server.recv(2048).decode('utf-8')
+                check_on_correctness = str(self.game.guess(self.guess)).encode('utf-8')
+                self.server.sendto(check_on_correctness, self.players[0])
+                self.server.sendto(check_on_correctness, self.players[1])
+            self.players[0], self.players[1] = self.players[1], self.players[0]
+            print(self.players)
+            self.server.sendto('1,2'.encode('utf-8'), self.players[0])
+            self.server.sendto('2,2'.encode('utf-8'), self.players[1])
+
     def main(self):
+        """Основной метод серверной части приложения. Создается сервер, затем в цикле начинаем принимать сообщения от
+        игроков и проверяем их на наличие в списке. Дальше смотрим, сколько игроков подключено. Если один, то отправляем
+        ему количество игроков и его номер, а затем ждем второго игрока. Когда подключится второй игрок, отправляем
+        обоим игрокам их номер и количество игроков в игре, после чего начинается игра."""
         self.create_server()
         while True:
             self.message, self.client_addr = self.server.recvfrom(2048)
@@ -44,24 +72,11 @@ class Server:
                 self.send_num = False
                 continue
 
+            # Обработка ожидания первым игроком второго, чтобы приложение корректно работало:
             if len(self.players) == 2 and self.message == "get_data":
                 continue
             elif len(self.players) == 2:
-                while True:
-                    self.number = self.server.recv(2048).decode('utf-8')
-                    self.game.reset_attributes(self.number)
-                    self.guess = None
-                    self.server.sendto('True'.encode('utf-8'), self.players[1])
-                    while not self.number == self.guess:
-                        self.guess = self.server.recv(2048).decode('utf-8')
-                        check_on_correctness = str(self.game.guess(self.guess)).encode('utf-8')
-                        self.server.sendto(check_on_correctness, self.players[0])
-                        self.server.sendto(check_on_correctness, self.players[1])
-                    self.players[0], self.players[1] = self.players[1], self.players[0]
-                    print(self.players)
-                    self.server.sendto('1,2'.encode('utf-8'), self.players[0])
-                    self.server.sendto('2,2'.encode('utf-8'), self.players[1])
-                    # break
+                self.play()
 
         self.server.close()
 
